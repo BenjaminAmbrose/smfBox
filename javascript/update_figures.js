44aa71d85f5a042e111e2b73b279c9b604f947e7
BenjaminAmbrose <37077982+BenjaminAmbrose@users.noreply.github.com>
2018-12-21 12:41:19 +0000

    Update update_figures.js

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -2,7 +2,7 @@
 // Figure Exploded interactively loads static images, stored locally
 function update_figure_exploded() {
   var period = document.getElementById("Figure_exploded_period_slider").value;
-  var filename = "./images/emission_path_explode_images/Explode_Emission_Path_smfScope_Final_" + period + ".png";
+  var filename = "images/emission_path_explode_images/Explode_Emission_Path_smfScope_Final_" + period + ".png";
   var image = document.getElementById("Figure_exploded_image");
   image.src = filename;
 }
@@ -16,7 +16,7 @@ function update_figure_exploded_ex() {
 
 function update_figure_exploded_box() {
   var period = document.getElementById("Figure_exploded_period_slider_box").value;
-  var filename = "./images/final_explode_images_microscope_body_smfscope/final_explode_images_microscope_body_" + period + ".png";
+  var filename = "images/final_explode_images_microscope_body_smfscope/final_explode_images_microscope_body_" + period + ".png";
   var image = document.getElementById("Figure_exploded_image_box");
   image.src = filename;
 }
@@ -31,7 +31,7 @@ function update_figure_rotated() {
   }else{
 	  id = 0;
   }
-  var filename = "./images/final_rotation_view_smfscope/final_rotation_view_smfscope_" + id + ".png";
+  var filename = "images/final_rotation_view_smfscope/final_rotation_view_smfscope_" + id + ".png";
   var image = document.getElementById("Figure_rotated_image");
   image.src = filename;
 }
