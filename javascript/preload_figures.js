6e9570fd29967df3fe6f262d1aa1d3dcf6c9a4f1
BenjaminAmbrose <37077982+BenjaminAmbrose@users.noreply.github.com>
2018-12-21 12:41:00 +0000

    Update preload_figures.js

--- javascript/preload_figures.js
+++ javascript/preload_figures.js
@@ -2,7 +2,7 @@ var images = [];
 function loadExplodedImages() {
     for (var i = 0; i < 40; i++) {
         images[i] = new Image();
-		var imsrc = './images/emission_path_explode_images/explode_emission_path_smfScope_Final_{i}.png';
+		var imsrc = 'images/emission_path_explode_images/explode_emission_path_smfScope_Final_{i}.png';
 		images[i].src = imsrc;
     }
 }
@@ -20,7 +20,7 @@ var images = [];
 function loadExplodedImagesBox() {
     for (var i = 0; i < 55; i++) {
         images[i] = new Image();
-		var imsrc = './images/final_explode_images_microscope_body_smfscope/final_explode_images_microscope_body_{i}.png';
+		var imsrc = 'images/final_explode_images_microscope_body_smfscope/final_explode_images_microscope_body_{i}.png';
 		images[i].src = imsrc;
     }
 }
@@ -29,7 +29,7 @@ var imagesr = [];
 function loadRotatedImages() {
     for (var i = 0; i < 35; i++) {
         imagesr[i] = new Image();
-		var imsrc = './images/final_rotation_view_smfscope/final_rotation_view_smfscope_{i}.png';
+		var imsrc = 'images/final_rotation_view_smfscope/final_rotation_view_smfscope_{i}.png';
 		imagesr[i].src = imsrc;
     }
 }
