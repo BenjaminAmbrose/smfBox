8ffafb577f0cb424a9d7ce2080c03cc964d5bdfe
BenjaminAmbrose <37077982+BenjaminAmbrose@users.noreply.github.com>
2019-05-28 14:32:48 +0100

    added new pages

--- header.js
+++ header.js
@@ -8,6 +8,8 @@ document.write('\
     <li class="first"><a href="./shopping list.html" target="_self">Shopping List</a></li>\
     <li class="first"><a href="./acqsoftware.html" target="_self">Operational Software</a></li>\
     <li class="first"><a href="./anasoftware.html" target="_self">Analysis Software</a></li>\
+    <li class="first"><a href="./glossary.html" target="_self">Glossary</a></li>\
+		<li class="first"><a href="./relatedprojects.html" target="_self">Related Projects</a></li>\
 		<li class="first"><a href="./people.html" target="_self">People involved</a></li>\
 		<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
 		<font style="line-height:1.5em;">\
